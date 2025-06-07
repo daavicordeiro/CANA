12f6d5d1b55c656b58858d653b54f13ad4185d00
daavicordeiro <121406578+daavicordeiro@users.noreply.github.com>
2025-06-07 17:57:43 -0300

    Create KnightTour.java

---
+++ KnightTour.java
@@ -0,0 +1,69 @@
+public class PasseioDoCavalo {
+
+    static final int N = 8;
+
+    static int[] moveX = {2, 1, -1, -2, -2, -1, 1, 2};
+    static int[] moveY = {1, 2, 2, 1, -1, -2, -2, -1};
+
+    public static void main(String[] args) {
+        int[][] tabuleiro = new int[N][N];
+
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < N; j++)
+                tabuleiro[i][j] = -1;
+
+        int linhaInicial = 0;
+        int colunaInicial = 0;
+        tabuleiro[linhaInicial][colunaInicial] = 0;
+
+        if (resolverPasseio(linhaInicial, colunaInicial, 1, tabuleiro)) {
+            
+            imprimirTabuleiro(tabuleiro);
+        } else {
+            System.out.println("Nenhuma solução encontrada.");
+        }
+    }
+
+    public static boolean resolverPasseio(int linha, int coluna, int numMovimento, int[][] tabuleiro) {
+        
+        if (numMovimento == N * N) {
+            return true;
+        }
+
+        
+        for (int i = 0; i < 8; i++) {
+            int proxLinha = linha + moveX[i];
+            int proxColuna = coluna + moveY[i];
+
+            
+            if (posicaoValida(proxLinha, proxColuna, tabuleiro)) {
+                tabuleiro[proxLinha][proxColuna] = numMovimento; 
+
+                
+                if (resolverPasseio(proxLinha, proxColuna, numMovimento + 1, tabuleiro)) {
+                    return true;
+                } else {
+                    
+                    tabuleiro[proxLinha][proxColuna] = -1;
+                }
+            }
+        }
+
+        
+        return false;
+    }
+
+    public static boolean posicaoValida(int linha, int coluna, int[][] tabuleiro) {
+        return (linha >= 0 && linha < N && coluna >= 0 && coluna < N && tabuleiro[linha][coluna] == -1);
+    }
+
+    public static void imprimirTabuleiro(int[][] tabuleiro) {
+        System.out.println("Solução encontrada!");
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                System.out.printf("%2d ", tabuleiro[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
